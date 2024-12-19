5d35b97b51030b4e60404b48d49c4f4cc2138c2c
Anna Rider <3461836+annarider@users.noreply.github.com>
2024-12-19 14:50:03 -0700

    Add loop logic to play 5 rounds
    Added loop for multiple rounds and refactored
    the playRounds logic to move some parts to
    playGame. Need to fix the return statements for
    playRound for full function.

--- playRockPaperScissors.js
+++ playRockPaperScissors.js
@@ -83,6 +83,7 @@ function playRound(humanChoice, computerChoice) {
     humanScore += 1;
     computerScore += 1;
     console.log('Tie! You both won.');
+    return 
   } else if (humanChoice.toLowerCase() === 'rock' && computerChoice === 'paper' ) {
     computerScore += 1;
     console.log("You lose! Paper beats Rock.");
@@ -103,6 +104,43 @@ function playRound(humanChoice, computerChoice) {
     console.log("You win! Scissors beat Paper.");
   }
 }
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-// console.log(getHumanChoice());
+
+/**
+ * playGame allows 4 rounds. It keeps track of scores
+ * and declares a winner at the end.
+ * 
+ * playGame starts a loop that runs 5 times
+ * for 5 rounds. 
+ * In 1 playRound: 
+ * playRound calls getHumanChoice &
+ * getComputerChoice to init the choices
+ * 
+ * call playRound & pass in init choices
+ * playRound determines the round's winner,
+ * returns a winner
+ * 
+ * playGame increments the round winner's score.
+ * 
+ * At the end of 5 rounds, it 
+ * announces a winner.
+ */
+
+function playGame() {
+  for (let step = 0; step < 5; step++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    let winner = playRound(humanSelection, computerSelection);
+    
+    if (winner === 'human') {
+      humanScore += 1;
+    } else if (winner === 'computer') {
+      computerScore += 1;
+    } else {
+      humanScore += 1;
+      computerScore += 1;
+    }
+    console.log("The winner is: " + winner);
+  }
+  
+
+}
